c4403560e18116ec1f4092da58d51533b4517c1a
JR-coderli <nan756504799@gmail.com>
2025-12-24 23:22:47 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173, // 这里就是指定端口的地方
+    strictPort: true, // 如果 5173 被占用，直接报错而不是寻找下一个可用端口
+    open: true, // 启动时自动打开浏览器
+  },
 });
